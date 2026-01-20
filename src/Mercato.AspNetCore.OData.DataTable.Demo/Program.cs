8b83a11ffdd91f4ec584ed849d8ab78193cbbc3a
Mark Middlemist <mark.middlemist@mercatosolutions.co.uk>
2026-01-20 09:04:50 +0000

    Net8 demo project added

---
+++ src/Mercato.AspNetCore.OData.DataTable.Demo/Program.cs
@@ -0,0 +1,26 @@
+namespace Mercato.AspNetCore.OData.DataTable.Demo;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
